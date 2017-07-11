31d8aac92d821683cdaf59955b863e0d3d4257da
Abdullah <abdullah.a@servicerocket.com>
2017-07-11 19:54:43 +0800

    implemented basic nightwatch testing up end of editor and save page

--- tests/test.js
+++ tests/test.js
@@ -5,12 +5,12 @@ module.exports = {
             .url('https://abba22.atlassian.net/wiki/display/')
             .waitForElementVisible('body', 1000)
             .setValue('input#username', 'sforce.rkt54@gmail.com')
-            .click('button#login-submit')
-            .waitForElementVisible('input#password', 1000)
+            .click('#login-submit')
+            .waitForElementVisible('input#password', 2000)
             .setValue('input#password', 'testuser')
             .click('button#login-submit')
-            .waitForElementVisible('body', 5000)
-            .pause(6000)
+            .waitForElementVisible('body', 7000)
+            .pause(10000)
             .click('#quick-create-page-button')
             .waitForElementVisible('body', 3000)
             .setValue('input#content-title', 'testdraft3232')
@@ -19,35 +19,47 @@ module.exports = {
             .click('#rte-button-insert')
             .click('#google-drive-connector')
             .pause(4000)
-            // .window_handles((res)=> {
-            //         console.log(res);
-            //         var handle = res.value[0];
-            // browser.switchWindow(handle);
-            // })
             .frame(1, (res)=>{console.log("switched to frame")})
-
             .pause(3000)
             .assert.elementPresent('div#editor')
             .click('#launch-picker')
             .pause(5000)
             .window_handles((res)=> {
-                    var handle = res.value[1];
-                    console.log(res);
+                var handle = res.value[1];
+                browser.switchWindow(handle)
+                .pause(2000)
+                .verify.elementPresent('div#view_container')
+                .setValue('input#identifierId', 'sforce.rkt54@gmail.com')
+                .sendKeys('input#identifierId', browser.Keys.ENTER)
+                .pause(6000)
+                .assert.elementPresent('div#password')
+                .setValue('input[name=password]', 'drK37sjFZh7')
+                .sendKeys('input[name=password]', browser.Keys.ENTER)
+                .pause(6000);
+                })
+            .pause(2000)
+            .window_handles((res)=> {
+                var handle = res.value[0];
                 browser.switchWindow(handle);
-                browser.assert.elementPresent('div#view_container');
-                browser.setValue('input#identifierId', 'sforce.rkt54@gmail.com');
-                browser.keys(browser.Keys.ENTER);
-                browser.pause(2000);
-            })
+                })
+            .verify.elementPresent('div#editor')
 
+            .verify.elementPresent('.picker-frame.picker-dialog-frame')
+            .pause(8000)
+            .elementActive().frame(2).frame(1)
+            .pause(2000)
+            .verify.elementPresent("div[data-target=doc]")
+            .click('div[data-target=doc]')
+            .pause(3000)
+            .verify.elementPresent("div[id^=\"picker:ap:0\"]")
+            .click("div[id^=\"picker:ap:0\"]")
             .pause(4000)
-
-            // .click('button#login-submit')
-            // .setValue('input#password', 'drK37sjFZh7')
-
-            .frame(1)
-
-            // .assert.containsText('#main', 'Night Watch')
+            .frame(null)
+            .pause(1000)
+            .click('#ap-button-0')
+            .pause(6000)
+            .click('#rte-button-publish')
+            .pause(10000)
             .end();
     }
 };
